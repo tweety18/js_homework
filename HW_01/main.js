3d60dc6bf9ef1b358af2cb9142d014e0e111cb35
Alona <alona.smetanina@raiffeisen.ua>
2025-01-06 23:27:01 +0100

    fixed 3 task with rows qty input

--- HW_01/main.js
+++ HW_01/main.js
@@ -1,25 +1,25 @@
-//1
-// Просте число — це натуральне число, яке має рівно два різні натуральні дільники (лише 1 і саме число).
-// Решту чисел, окрім одиниці та нуля, називають складеними.
-// Натуральні числа - це числа, які використовують при лічбі.
-
-const getData = prompt('Введіть натуральне число:');
-const getNumber = +getData;
-const isBiggerThan1 = getNumber > 1;
-
-if (isNaN(getNumber) || !isBiggerThan1) {
-    console.log('Виникла помилка обчислення');
-} else  {
-    let counter = 0;
-    for (let i = 1; i <= getNumber; i++) {
-        if (getNumber % i === 0) {
-            counter += 1;
-        }
-    }
-    counter === 2 ? console.log(`Число ${getNumber} просте`) : console.log(`Число ${getNumber} складене`);
-}
-
-// 2
+// //1
+// // Просте число — це натуральне число, яке має рівно два різні натуральні дільники (лише 1 і саме число).
+// // Решту чисел, окрім одиниці та нуля, називають складеними.
+// // Натуральні числа - це числа, які використовують при лічбі.
+//
+// const getData = prompt('Введіть натуральне число:');
+// const getNumber = +getData;
+// const isBiggerThan1 = getNumber > 1;
+//
+// if (isNaN(getNumber) || !isBiggerThan1) {
+//     console.log('Виникла помилка обчислення');
+// } else  {
+//     let counter = 0;
+//     for (let i = 1; i <= getNumber; i++) {
+//         if (getNumber % i === 0) {
+//             counter += 1;
+//         }
+//     }
+//     counter === 2 ? console.log(`Число ${getNumber} просте`) : console.log(`Число ${getNumber} складене`);
+// }
+//
+// // 2
 // досконале число  — натуральне число, що дорівнює сумі його додатних дільників, не враховуючи самого числа.
 // Наприклад, 6 має дільники 1, 2, 3 (не враховуючи його самого),
 // 6 = 1 + 2 + 3, тому 6 — досконале число.
@@ -58,24 +58,17 @@ if (isNaN(getNumber) || !isBiggerthan1) {
 
 
 //3
-const getData = prompt('Введіть число більше або дорівнює 4:');
+const getData = prompt('Введіть кількість рядків більше або дорівнює 2:');
 const getNumber = +getData;
-const isMinimalNumber = getNumber >= 4; //for minimal piramide
+const isMinimalRows = getNumber >= 2; //for minimal piramide
 let pyramidString = '';
 
-if (isNaN(getNumber) || !isMinimalNumber) {
+if (isNaN(getNumber) || !isMinimalRows) {
     console.log('Pyramid can not be created');
 } else {
-    let starsUsed = 0;
-    let i = 1;
-    while (starsUsed + (2 * i - 1) <= getNumber) {
-        starsUsed += 2 * i - 1;
-        i++;
-    }
 
-    let rows = i - 1;
-    for (let j = 1; j <= rows; j++) {
-        let spaces = ' '.repeat(rows - j);
+    for (let j = 1; j <= getNumber; j++) {
+        let spaces = ' '.repeat(getNumber - j);
         let stars = '*'.repeat(2 * j - 1);
         pyramidString += spaces + stars + '\n';
     }
