23b53e3720d68b1ebee793c39e280df206e72bd8
Alona <alona.smetanina@raiffeisen.ua>
2025-01-08 21:28:09 +0100

    add homework

--- HW_01/main.js
+++ HW_01/main.js
@@ -1,1 +1,26 @@
-console.log('Hey, this is my rst JS program');
+// 1
+function reverseString(str) {
+    let newStr = ''
+    for (let i = str.length - 1; i >= 0; i--) {
+        newStr += str[i];
+    }
+    return newStr;
+}
+
+console.log(reverseString('hello'));
+
+
+// 2
+// Больше всего пятибуквенных слов-палиндромов: заказ, довод, кабак, казак, комок, наган, потоп, ротор, топот, шабаш, шалаш.
+function isPalindrome(str) {
+
+    let reverseStr = '';
+    for (let i = str.length - 1; i >= 0; i--) {
+        reverseStr += str[i];
+    }
+    return reverseStr === str ? `${str} is palindrome`: ` ${str} is not palindrome`;
+}
+
+console.log(isPalindrome('test'));
+console.log(isPalindrome('шалаш'))
+
