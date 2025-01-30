192d767d694d40e851e707ae9b754dfe222675fb
Alona <alona.smetanina@raiffeisen.ua>
2025-01-30 10:54:04 +0100

    add hw

--- HW_01/main.js
+++ HW_01/main.js
@@ -1,1 +1,36 @@
-console.log('Hey, this is my rst JS program');
+//1
+function decorator(fn) {
+    return function (...args) {
+        console.log(args);
+        return fn.apply(this, args);
+    }
+}
+
+function infinitArgs(...args) {
+    return args;
+}
+
+const decoratedFunction = decorator(infinitArgs);
+decoratedFunction(1,2,3,4);
+
+// 2
+function validate(sum, validator) {
+    return function (...args) {
+        if (args.find(el => el < validator)) {
+            throw new Error('there is negative number in arguments')
+        }
+            console.log('Validation passed succesfully');
+            return sum.apply(this, args);
+    }
+}
+
+function sum(...args) {
+    return args;
+}
+
+const validateSum = validate(sum, 0);
+validateSum(1,2,3);
+validateSum(1,-2,3);
+
+
+
